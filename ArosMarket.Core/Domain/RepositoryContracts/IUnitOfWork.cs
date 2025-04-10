0a34df1e2a368e948b2ae77d6d8517ecccd844ad
asadbek20005-rgb <154343160+asadbek20005-rgb@users.noreply.github.com>
2025-04-10 17:25:58 +0500

    changes

--- ArosMarket.Core/Domain/RepositoryContracts/IUnitOfWork.cs
+++ ArosMarket.Core/Domain/RepositoryContracts/IUnitOfWork.cs
@@ -1,25 +1,12 @@
-﻿using ArosMarket.Core.Domain.Entites;
-using ArosMarket.Core.Domain.RepositoryContracts.BaseContracts;
+﻿using ArosMarket.Core.Domain.RepositoryContracts.BaseContracts;
 
 namespace ArosMarket.Core.Domain.RepositoryContracts;
 
-internal interface IUnitOfWork : IDisposable
+public interface IUnitOfWork<TEntity> where TEntity : class, IDisposable
 {
-    IBaseRepository<User> UserRepository { get; }
-    IBaseRepository<Product> ProductRepository { get; }
-    IBaseRepository<Content> ContentRepository { get; }
-    IBaseRepository<ContentType> ContentTypeRepository { get; }
-    IBaseRepository<ProductType> ProductTypeRepository { get; }
-    IBaseRepository<Currency> CurrencyRepository { get; }
-    IBaseRepository<Gender> GenderRepository { get; }
-    IBaseRepository<Role> RoleRepository { get; }
-    IBaseRepository<UserStatus> UserStatusRepository { get; }
-    IBaseRepository<ProductStatus> ProductStatusRepository { get; }
-    IBaseRepository<Order> OrderRepository { get; }
-    IBaseRepository<OrderItem> OrderItemRepository { get; }
-    IBaseRepository<Payment> PaymentRepostory { get; }
-    IBaseRepository<PaymentMethod> PaymentMethodRepository { get; }
-    IBaseRepository<PaymentStatus> PaymentStatusRepository { get; }
-
+    IBaseRepository<TEntity> Repository { get; }
     Task<int> CompleteAsync();
+    Task BeginTransactionAsync();
+    Task CommitAsync();
+    Task RollbackAsync();
 }
---
+++ ArosMarket.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿
+using ArosMarket.Core.Domain.RepositoryContracts;
+using ArosMarket.Core.Domain.RepositoryContracts.BaseContracts;
+using ArosMarket.Infrastructure.DbContext;
+
+namespace ArosMarket.Infrastructure.Repositories;
+
+public class UnitOfWOrk<TEntity>(ArosMarketDbContext arosMarketDbContext) : IUnitOfWork<TEntity> where TEntity : class, IDisposable
+{
+    private readonly ArosMarketDbContext _context = arosMarketDbContext;
+    private IBaseRepository<TEntity>? _repository;
+    public IBaseRepository<TEntity> Repository
+    {
+        get
+        {
+            if (_repository == null)
+            {
+                _repository = new BaseRepository<TEntity>(_context);
+            }
+            return _repository;
+        }
+    }
+
+    public async Task BeginTransactionAsync()
+    {
+        await _context.Database.BeginTransactionAsync();
+    }
+
+    public async Task CommitAsync()
+    {
+        await _context.Database.CommitTransactionAsync();
+    }
+
+    public async Task<int> CompleteAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
+
+    public async Task RollbackAsync()
+    {
+        await _context.Database.RollbackTransactionAsync();
+    }
+}
